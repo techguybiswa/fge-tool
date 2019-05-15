1b87cd4e9fed8c3c2b97b3876097d6bcc84d6088
Biswarup Banerjee <bis.banerjee.bb@gmail.com>
2019-05-15 18:21:00 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import WelcomeHeader from './components/WelcomeHeader'
+
+function App() {
+  return (
+    <div className="App">
+    <WelcomeHeader/>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
